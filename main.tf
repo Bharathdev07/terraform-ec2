0c63dccd398e9932672f0680d5a592d9d9182460
Bharathdev07 <devops854@gmail.com>
2024-08-08 20:38:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "vm_3" {
   ami                    = "ami-0ad21ae1d0696ad58"
-  instance_type          = "t2.micro"
+  instance_type          = "t2.medium"
   key_name               = "new" # The key_name should be the name of your key pair, not the file name
   subnet_id              = "subnet-00e5483cd17ed530d"
   vpc_security_group_ids = ["sg-0f9084fa653acbfda"] # Use a list for security group IDs
