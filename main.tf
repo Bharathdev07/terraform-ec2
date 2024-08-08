e1f16b50216daea656069a4a468e006fb41e66fb
Bharathdev07 <devops854@gmail.com>
2024-08-08 20:24:16 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "vm_3" {
+  ami                    = "ami-0ad21ae1d0696ad58"
+  instance_type          = "t2.micro"
+  key_name               = "new-key" # The key_name should be the name of your key pair, not the file name
+  subnet_id              = "subnet-00e5483cd17ed530d"
+  vpc_security_group_ids = ["sg-0f9084fa653acbfda"] # Use a list for security group IDs
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "production" # Use 'Name' for standard tag key
+  }
+}
